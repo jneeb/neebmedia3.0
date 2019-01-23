7bbb8d596f3606bd4ca451b726b8bd1af7810ab0
Justin Neeb <neebjustin@gmail.com>
2019-01-23 15:28:54 -0500

    init commit

---
+++ site/functions/burger.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".mobileBurger").click(function () {
+        if($(".mobileBurger").hasClass("isOpen")) {
+            $(this).removeClass("isOpen");
+            $(".mobileSlider").removeClass("isOpen");
+        } else {
+            $(this).addClass("isOpen");
+            $(".mobileSlider").addClass("isOpen");
+        }
+    });
+});
